42c8c2ce2a6c59caeb8c3bba4d61f806a81b4d51
Selena-Sun <sunselena66@yahoo.com>
2025-07-16 04:07:34 +0000

    1

--- app.py
+++ app.py
@@ -54,18 +54,6 @@ def prediction():
 
 @app.route("/telegram",methods=["GET","POST"])
 def telegram():
-    domain_url = 'https://groq-dbs-prediction.onrender.com'
-
-    # The following line is used to delete the existing webhook URL for the Telegram bot
-    delete_webhook_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/deleteWebhook"
-    requests.post(delete_webhook_url, json={"url": domain_url, "drop_pending_updates": True})
-
-    # Set the webhook URL for the Telegram bot
-    set_webhook_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/setWebhook?url={domain_url}/webhook"
-    webhook_response = requests.post(set_webhook_url, json={"url": domain_url, "drop_pending_updates": True})
-
-@app.route("/telegram",methods=["GET","POST"])
-def telegram():
 
     domain_url = 'https://groq-dbs-prediction-sa0y.onrender.com'
 

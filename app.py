97d906c0cc57c2b9ac4019b5cade86117a26511b
Selena-Sun <sunselena66@yahoo.com>
2025-07-16 03:31:48 +0000

    1

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 from groq import Groq
 
 import os
-os.environ['GROQ_API_KEY'] = os.getenv("groq")
+# os.environ['GROQ_API_KEY'] = os.getenv("GROQ_API_KEY")
 # for cloud ..........
 
 app = Flask(__name__)
@@ -50,6 +50,39 @@ def prediction():
     # make prediction
     pred = model.predict([[q]])
     return(render_template("prediction.html",r=pred))
+
+@app.route("/telegram",methods=["GET","POST"])
+def telegram():
+    domain_url = 'https://groq-dbs-prediction.onrender.com'
+
+    # The following line is used to delete the existing webhook URL for the Telegram bot
+    delete_webhook_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/deleteWebhook"
+    requests.post(delete_webhook_url, json={"url": domain_url, "drop_pending_updates": True})
+
+    # Set the webhook URL for the Telegram bot
+    set_webhook_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/setWebhook?url={domain_url}/webhook"
+    webhook_response = requests.post(set_webhook_url, json={"url": domain_url, "drop_pending_updates": True})
+
+    @app.route("/telegram",methods=["GET","POST"])
+def telegram():
+
+    domain_url = 'https://groq-dbs-prediction-sa0y.onrender.com'
+
+    # The following line is used to delete the existing webhook URL for the Telegram bot
+    delete_webhook_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/deleteWebhook"
+    requests.post(delete_webhook_url, json={"url": domain_url, "drop_pending_updates": True})
+
+    # Set the webhook URL for the Telegram bot
+    set_webhook_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/setWebhook?url={domain_url}/webhook"
+    webhook_response = requests.post(set_webhook_url, json={"url": domain_url, "drop_pending_updates": True})
+
+    if webhook_response.status_code == 200:
+        # set status message
+        status = "The telegram bot is running. Please check with the telegram bot. @@dsai_selena_bot"
+    else:
+        status = "Failed to start the telegram bot. Please check the logs."
+    
+    return(render_template("telegram.html", status=status))
 
 if __name__ == "__main__":
     app.run()
